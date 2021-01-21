2e3839ea7e3e0d4dd87d4233a09be4a94aba69bf
abasaltr <abasaltian1@yahoo.com>
2021-01-21 11:41:04 -0600

    add JS app to JSON data

---
+++ baseball-v1/sandbox/reza/static/js/app.js
@@ -0,0 +1,46 @@
+// PROJECT CODE HERE!
+console.log('This is the Baseball-v1 dashboard js file');
+
+// Fetch the JSON data and call function init()
+var url_franchises = "/api/franchises";
+var url_teams = "/api/teams";
+var url_team_stats = "/api/team-stats";
+// .
+// ..
+// ...
+var urls = [url_franchises, url_teams, url_team_stats]
+
+var promises = [];
+urls.forEach(function (url) { promises.push(d3.json(url)) });
+console.log(promises);
+Promise.all(promises).then(data => init(data));
+// end JSON Fetch
+
+// function addFranchises
+function addFranchises(response) {
+    var franchises = response;
+    return franchises;
+}//end addFranchises() function
+
+// function addTeams
+function addTeams(response) {
+    var teams = response;
+    return teams;
+}//end addCensusCrime() function
+
+// function addTeamStats
+function addTeamStats(response) {
+    var team_stats = response;
+    return team_stats;
+}//end addTeamStats() function
+
+
+// function init
+function init(data) {
+
+    // initialize the data for the elements
+    franchisesIn = addFranchises(data[0]);
+    teamsIn = addTeams(data[1]);
+    team_statsIn = addTeamStats(data[2]);
+
+}//end init() function
